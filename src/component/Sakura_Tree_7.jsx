1a46e5fa64c241d165e3fb0bdd42d4690924ceac
DioAndrew <86814835+DioAndrew@users.noreply.github.com>
2025-01-23 14:37:02 +0700

    update sakura.jsx, package.json

--- src/component/Sakura_Tree_7.jsx
+++ src/component/Sakura_Tree_7.jsx
@@ -13,15 +13,15 @@ export function SakuraTree(props) {
   // useHelper(light, SpotLightHelper, "red")
 
   const GrassTexture = useTexture({
-    map: '/textures/grass/grass_diff.jpg',
+    map: './textures/grass/grass_diff.jpg',
   })
   const WoodTexture = useTexture({
     map: '/textures/wood/wood_diff.jpg',
-    displacementMap: '/textures/wood/wood_disp.png',
+    displacementMap: './textures/wood/wood_disp.png',
   })
 
   const RoadTexture = useTexture({
-    map: '/textures/road/road_diff.jpg',
+    map: './textures/road/road_diff.jpg',
   })
 
   return (
